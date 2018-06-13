d3a2b77659bf2562bd9642158743222392900159
David Broadfoot <david.broadfoot@smokeball.com>
2018-06-13 16:56:58 +1000

    Change to NET Framework

--- src/LaunchDarkly.EventSource/Properties/AssemblyInfo.cs
+++ src/LaunchDarkly.EventSource/Properties/AssemblyInfo.cs
@@ -1,4 +1,36 @@
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-// Added to allow the Test Project to access internal types and methods.
-[assembly: InternalsVisibleTo("LaunchDarkly.EventSource.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010015ba095c5a95acefa557867cec3f488906ec0ef6fe6728a7cfdeef861fcce49ea79357ba825d95d56d67597bc9cc9a473438f5607908186fc477fdeafc68f387552061ebf57d6e585317d5047a57bd496034ff854a417236776003bcba328fa8bf4a024c4d212ba4fb4033ebfb14116c12cde63d16551b9f48c20ee54a417deb")]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SB-LaunchDarkly.EventSource")]
+[assembly: AssemblyDescription("Smokeball")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Smokeball")]
+[assembly: AssemblyProduct("SB-LaunchDarkly.EventSource")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3f7c8356-3816-4577-9e40-ca083d049173")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.1.1.0")]
+[assembly: AssemblyFileVersion("3.1.1.0")]
