65311011453d217b111fae763be430e1a7754ed8
David Broadfoot <david.broadfoot@smokeball.com>
2018-06-14 09:24:19 +1000

    Bump version

--- src/LaunchDarkly.EventSource/Properties/AssemblyInfo.cs
+++ src/LaunchDarkly.EventSource/Properties/AssemblyInfo.cs
@@ -13,6 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyInformationalVersion("3.1.1.1")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -32,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.1.0")]
-[assembly: AssemblyFileVersion("3.1.1.0")]
+[assembly: AssemblyVersion("3.1.1.1")]
+[assembly: AssemblyFileVersion("3.1.1.1")]
